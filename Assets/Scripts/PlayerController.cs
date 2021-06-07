ac2b256885b3ca2c939dc1eda67a44f443edd161
VictorBwD <victor.bogdanowdornelles@gmail.com>
2021-06-07 20:26:57 -0300

    Movimentação e pulo feitas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpHeight;
+    public bool isGrounded;
     private Rigidbody rb;
 
     private void Start()
@@ -18,10 +19,32 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * speed * Time.deltaTime);
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector3.up * jumpHeight);
+            }
+        }
+        
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space)){
-            rb.AddForce(Vector3.up * jumpHeight);
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+            Debug.Log("Enter");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.name == "Ground")
+        {
+            isGrounded = false;
+            Debug.Log("Exited");
         }
     }
 }
